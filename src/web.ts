11394abeaab5b9e10d02f89fe6bba5c21bdc77bc
Sylvie <sylvie@tycrek.com>
2025-02-07 22:43:42 -0700

    feat: accept uploads (not being put in db yet)

--- src/web.ts
+++ src/web.ts
@@ -1,5 +1,7 @@
 import { Hono } from '@hono/hono';
-import { WHO_AM_I } from './utils.ts';
+import { crypto } from '@std/crypto';
+import { encodeHex } from '@std/encoding/hex';
+import { join, log, WHO_AM_I } from './utils.ts';
 
 // Set up Hono
 const app = new Hono();
@@ -7,10 +9,23 @@ const app = new Hono();
 app.get('/', (ctx) => ctx.text(WHO_AM_I));
 
 app.post('/upload', async (ctx) => {
-	const file = (await ctx.req.formData()).get('file') as File;
-	console.log(file.name, file);
-	ctx.status(400);
-	return ctx.text('not implemented');
+	const body = await ctx.req.formData();
+
+	if (!body.has('file')) {
+		ctx.status(400);
+		return ctx.text('file parameter missing');
+	}
+
+	// Get file from body
+	const file = body.get('file') as File;
+
+	const uid = encodeHex(await crypto.subtle.digest('BLAKE3', new TextEncoder().encode(`${file.name}${Date.now()}`)));
+	const filename = `${uid}.${file.name.split('.').pop()}`;
+
+	// Save file to disk
+	await Deno.writeFile(join('data/uploads', filename), file.stream());
+
+	return ctx.json({ url: `http://wip/tbd` });
 });
 
 export default app.fetch;

f9e620023fb715e3d864d47bdb24665818d871bc
Sylvie <sylvie@tycrek.com>
2025-02-07 20:42:38 -0700

    feat: implement basic FormData handling

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,12 @@ import Log from './Log.ts';
 import pkg from '../deno.json' with { type: 'json' };
 import * as path from '@std/path';
 
+export const WHO_AM_I = `${pkg.name.split('/')[1]} v${pkg.version}`;
+
 /**
  * Logger
  */
-export const log = new Log(`${pkg.name.split('/')[1]} v${pkg.version} |`);
+export const log = new Log(`${WHO_AM_I} |`);
 
 /**
  * Path joiner
--- src/web.ts
+++ src/web.ts
@@ -1,8 +1,16 @@
 import { Hono } from '@hono/hono';
+import { WHO_AM_I } from './utils.ts';
 
 // Set up Hono
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Deno!'));
+app.get('/', (ctx) => ctx.text(WHO_AM_I));
+
+app.post('/upload', async (ctx) => {
+	const file = (await ctx.req.formData()).get('file') as File;
+	console.log(file.name, file);
+	ctx.status(400);
+	return ctx.text('not implemented');
+});
 
 export default app.fetch;

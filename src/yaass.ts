68760341bb1e1198a9f4d38c35e74d1d950d2997
Sylvie <sylvie@tycrek.com>
2025-02-07 19:09:12 -0700

    fix: ensure data dir exists

--- src/yaass.ts
+++ src/yaass.ts
@@ -1,8 +1,11 @@
 import { Hono } from '@hono/hono';
 import { Database } from '@db/sqlite';
+import { ensureDirSync } from '@std/fs';
+
+ensureDirSync('data');
 
 const app = new Hono();
-const db = new Database('yaass.db');
+const db = new Database('data/yaass.db');
 
 app.get('/', (c) => c.text('Hello Deno!'));
 

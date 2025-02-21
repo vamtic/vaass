275521871c113bc5df8e444558cf0fcf09602387
Sylvie <sylvie@tycrek.com>
2025-02-20 22:46:42 -0700

    feat: use new components

--- src/web/components/Dashboard.tsx
+++ src/web/components/Dashboard.tsx
@@ -1,14 +1,11 @@
 import { WEBSITE, WHO_AM_I } from '../../utils.ts';
-import { StylesLink } from './Styles.tsx';
+import Head from './Head.tsx';
 import type { Upload } from '../../types/Upload.ts';
 
 export default () => {
 	return (
 		<html>
-			<head>
-				<title>Dashboard</title>
-				<StylesLink />
-			</head>
+			<Head title='Dashboard' />
 		</html>
 	);
 };
--- src/web/components/Login.tsx
+++ src/web/components/Login.tsx
@@ -1,25 +1,27 @@
-import { WEBSITE, WHO_AM_I } from '../../utils.ts';
-import { StylesLink } from './Styles.tsx';
-import type { Upload } from '../../types/Upload.ts';
+import { WHO_AM_I } from '../../utils.ts';
+import Head from './Head.tsx';
+import Outlink from './Outlink.tsx';
 
 export default () => {
 	return (
 		<html>
-			<head>
-				<title>Login</title>
-				<StylesLink />
-			</head>
-			<body class='h-full flex flex-col flex-center'>
+			<Head title='Login' />
+			<body class='h-full flex flex-center'>
 				<div class='w-min rounded-2xl p-8 dark:bg-stone-800'>
-					<h1 class='text-2xl font-bold'>Login</h1>
+					<Outlink>
+						<img src='/favicon.png'></img>
+					</Outlink>
 					<form method='post'>
-						<p class='text-xl mt-4'>Username</p>
+						<p class='text-xl mt-8'>Username</p>
 						<input type='text' autofocus class='p-0.5 border rounded border-rose-500'></input>
 						<p class='text-xl mt-4'>Password</p>
 						<input type='password' class='p-0.5 border rounded border-rose-500'></input>
 						<div class='flex justify-around mt-4 gap-4 px-2'>
 							<button class='button basis-full' type='submit'>Login</button>
 							<button class='button basis-full'>Register</button>
+						</div>
+						<div class='mt-4 text-sm italic text-center'>
+							<Outlink />
 						</div>
 					</form>
 				</div>

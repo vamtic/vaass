4df6adf5160bac4aa713bcb6f71440b9782605b4
Sylvie <sylvie@tycrek.com>
2025-02-20 23:27:01 -0700

    refactor: index & favicon routes

---
+++ src/web/routes/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from '@hono/hono';
+import { join, WHO_AM_I } from '../../utils.ts';
+
+const route = new Hono();
+
+route.get('/', async (ctx) => {
+	// favicon loader
+	if (ctx.req.url.endsWith('.ico') || ctx.req.url.endsWith('.png')) {
+		const isPng = ctx.req.url.endsWith('.png');
+		ctx.header('Content-Type', isPng ? 'image/png' : 'image/x-icon');
+		return ctx.body(await Deno.readFile(join(isPng ? 'assets/yaass-logo.png' : 'assets/yaass.ico')));
+	}
+
+	return ctx.text(WHO_AM_I);
+});
+
+export default route;
--- src/web/server.ts
+++ src/web/server.ts
@@ -1,19 +1,15 @@
 import { Hono } from '@hono/hono';
-import { join, WHO_AM_I } from '../utils.ts';
+import index from './routes/index.ts';
 
 const app = new Hono();
 
 // domain middleware
 app.use((ctx, next) => (ctx.set('domain', new URL(ctx.req.url).origin), next()));
 
-app.get('/favicon.ico', async (ctx) => {
-	ctx.header('Content-Type', 'image/x-icon');
-	return ctx.body(await Deno.readFile(join('assets/yaass.ico')));
-});
-app.get('/favicon.png', async (ctx) => {
-	ctx.header('Content-Type', 'image/png');
-	return ctx.body(await Deno.readFile(join('assets/yaass-logo.png')));
-});
+// defaults
+app.route('/', index);
+app.route('/favicon.ico', index);
+app.route('/favicon.png', index);
 
 // routes
 app.route('/stylesheet.css', (await import('./routes/stylesheet.css.ts')).default);
@@ -21,6 +17,5 @@ app.route('/dashboard', (await import('./routes/dashboard.ts')).default);
 app.route('/login', (await import('./routes/login.ts')).default);
 app.route('/upload', (await import('./routes/upload.ts')).default);
 app.route('/', (await import('./routes/needle.ts')).default);
-app.get('/', (ctx) => ctx.text(WHO_AM_I));
 
 export default app.fetch;

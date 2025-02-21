61aefdb86117a378c011442abd803786176d4691
Sylvie <sylvie@tycrek.com>
2025-02-20 22:46:46 -0700

    feat: serve icons

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,10 +1,19 @@
 import { Hono } from '@hono/hono';
-import { WHO_AM_I } from '../utils.ts';
+import { join, WHO_AM_I } from '../utils.ts';
 
 const app = new Hono();
 
 // domain middleware
 app.use((ctx, next) => (ctx.set('domain', new URL(ctx.req.url).origin), next()));
+
+app.get('/favicon.ico', async (ctx) => {
+	ctx.header('Content-Type', 'image/image/x-icon');
+	return ctx.body(await Deno.readFile(join('assets/yaass.ico')));
+});
+app.get('/favicon.png', async (ctx) => {
+	ctx.header('Content-Type', 'image/png');
+	return ctx.body(await Deno.readFile(join('assets/yaass-logo.png')));
+});
 
 // routes
 app.route('/stylesheet.css', (await import('./routes/stylesheet.css.ts')).default);

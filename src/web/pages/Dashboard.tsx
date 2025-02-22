0ad04d1a801e5c94249a996ec1c55f4c26c1a1a9
Sylvie <sylvie@tycrek.com>
2025-02-22 00:18:51 -0700

    feat: empty dashboard page

--- src/web/pages/Dashboard.tsx
+++ src/web/pages/Dashboard.tsx
@@ -6,8 +6,17 @@ export default (user: User) => {
 	return (
 		<html>
 			<Head title='Dashboard' />
-			<body>
-				<h1>Welcome, {user.name}</h1>
+			<body class='h-full flex flex-row'>
+				<div class='h-full flex-none flex flex-col justify-center items-center p-16 gap-8'>
+					<i class='icon not-italic'>A</i>
+					<i class='icon not-italic'>B</i>
+					<i class='icon not-italic'>C</i>
+				</div>
+				<div class='h-screen w-screen flex pr-16 py-16'>
+					<div class='w-full p-8 flex flex-col overflow-scroll rounded-2xl dark:bg-stone-800'>
+						<h1 class='font-bold text-7xl'>Welcome, {user.name}</h1>
+					</div>
+				</div>
 			</body>
 		</html>
 	);

c36e79844314f3cf585d574e8b3c1c4a80cece24
Sylvie Fox <sylvie@tycrek.com>
2025-06-05 15:58:57 -0600

    feat: improve table

--- src/web/pages/Admin.tsx
+++ src/web/pages/Admin.tsx
@@ -6,11 +6,10 @@ export default () => {
 		<html>
 			<Head title='yaass admin' />
 			<body class='p-4'>
-				<h1 class='text-7xl'>Admin (WIP)</h1>
-				<div class='flex flex-col md:flex-row'>
+				<h1 class='text-5xl mb-2'>Admin (WIP)</h1>
+				<div class='flex flex-col md:flex-row gap-4'>
 					<div class='basis-full md:basis-1/2'>
-						<h2 class='text-4xl'>Users</h2>
-						<div class='dark:bg-stone-800 rounded-2xl p-4 my-2'>
+						<div class='dark:bg-stone-800 rounded-2xl p-4 my-4'>
 							<strong class='font-bold text-xl mb-2'>New user</strong>
 							<form class='grid grid-cols-2 gap-2 text-left' id='new-user' action='/register' method='post'>
 								<input name='username' type='text' autofocus class='input-text'></input>
@@ -22,28 +21,27 @@ export default () => {
 								<button class='button basis-full font-bold' type='submit'>Register</button>
 							</form>
 						</div>
-						<div class='dark:bg-stone-800 table-auto p-4 rounded-2xl'>
-							<table class='w-full table-auto'>
-								<thead>
+						<table class='dark:bg-stone-800 w-full table-auto border-collapse border border-stone-700'>
+							<thead>
+								<tr>
+									<th class='p-2 border border-stone-700'>ID</th>
+									<th class='p-2 border border-stone-700'>Name</th>
+									<th class='p-2 border border-stone-700'>Username</th>
+									<th class='p-2 border border-stone-700'>UID</th>
+								</tr>
+							</thead>
+							<tbody>
+								{DB.getUsers().map((user) => (
 									<tr>
-										<th>Name</th>
-										<th>Username</th>
-										<th>UID</th>
-										<th>Owner?</th>
+										<td class='font-mono font-bold p-2 border border-stone-700'>{user._id}</td>
+										<td class={`p-2 border border-stone-700 ${user.owner ? 'font-bold italic' : ''}`}>{user.name}</td>
+										<td class='p-2 border border-stone-700'>{user.username}</td>
+										<td class='font-mono p-2 border border-stone-700'>{user.uid}</td>
 									</tr>
-								</thead>
-								<tbody>
-									{DB.getUsers().map((user) => (
-										<tr>
-											<td>{user.name}</td>
-											<td>{user.username}</td>
-											<td class='font-mono'>{user.uid}</td>
-											{user.owner ? <td class='font-bold'>Yes</td> : <td></td>}
-										</tr>
-									))}
-								</tbody>
-							</table>
-						</div>
+								))}
+							</tbody>
+						</table>
+						{/* </div> */}
 					</div>
 					<div class='basis-full md:basis-1/2'>
 						<h2 class='text-4xl'>Stats</h2>

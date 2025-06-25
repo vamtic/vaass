b1018437de040fb4348f5e716f20d09fe1c5af63
vamtic <93308982+vamtic@users.noreply.github.com>
2025-06-25 19:00:22 +0200

    Vamticizmus, CSS fix

--- src/web/pages/LoginRegister.tsx
+++ src/web/pages/LoginRegister.tsx
@@ -15,9 +15,9 @@ export default ({ mode, page, error }: PropsWithChildren<{ mode: 'login' | 'regi
 				<script src='/login/swap.js' />
 			</Head>
 			<body class='h-full flex flex-center'>
-				<div class='w-min rounded-2xl p-8 dark:bg-stone-800'>
+				<div class='login-container'>
 					<Outlink>
-						<img class='mb-4' src='/favicon.png' />
+						<img class='mb-4 mx-auto' src='/favicon.png' />
 					</Outlink>
 
 					{/* Hibaüzenet megjelenítése, ha van */}
@@ -25,11 +25,15 @@ export default ({ mode, page, error }: PropsWithChildren<{ mode: 'login' | 'regi
 
 					{/* Bejelentkezési űrlap */}
 					<form class={mode == 'login' ? '' : 'hidden'} id='login' action={`/login?page=${page ?? 'dashboard'}`} method='post'>
-						<p class='text-xl mt-4'>Felhasználónév</p>
-						<input name='username' type='text' autofocus class='input-text'></input>
-						<p class='text-xl mt-4'>Jelszó</p>
-						<input name='password' type='password' class='input-text'></input>
-						<div class='flex justify-around mt-4 gap-4 px-2'>
+						<div class='form-row'>
+							<p class='text-xl'>Felhasználónév</p>
+							<input name='username' type='text' autofocus class='input-text'></input>
+						</div>
+						<div class='form-row'>
+							<p class='text-xl mt-4'>Jelszó</p>
+							<input name='password' type='password' class='input-text'></input>
+						</div>
+						<div class='button-group'>
 							<button class='button basis-full font-bold' type='submit'>Bejelentkezés</button>
 							<button class='button basis-full' type='button' onclick='swap();'>
 								Regisztráció
@@ -39,13 +43,19 @@ export default ({ mode, page, error }: PropsWithChildren<{ mode: 'login' | 'regi
 
 					{/* Regisztrációs űrlap */}
 					<form class={mode == 'register' ? '' : 'hidden'} id='register' action='/register' method='post'>
-						<p class='text-xl mt-4'>Felhasználónév</p>
-						<input name='username' type='text' autofocus class='input-text'></input>
-						<p class='text-xl mt-4'>Jelszó</p>
-						<input name='password' type='password' class='input-text'></input>
-						<p class='text-xl mt-4'>Jelszó megerősítése</p>
-						<input name='password2' type='password' class='input-text'></input>
-						<div class='flex justify-around mt-4 gap-4 px-2'>
+						<div class='form-row'>
+							<p class='text-xl'>Felhasználónév</p>
+							<input name='username' type='text' autofocus class='input-text'></input>
+						</div>
+						<div class='form-row'>
+							<p class='text-xl mt-4'>Jelszó</p>
+							<input name='password' type='password' class='input-text'></input>
+						</div>
+						<div class='form-row'>
+							<p class='text-xl mt-4'>Jelszó megerősítése</p>
+							<input name='password2' type='password' class='input-text'></input>
+						</div>
+						<div class='button-group'>
 							<button class='button basis-full' type='button' onclick='swap();'>Bejelentkezés</button>
 							<button class='button basis-full font-bold' type='submit'>Regisztráció</button>
 						</div>

aa2cbe06927faf4f09124ef56e6133226c6dfa42
vamtic <93308982+vamtic@users.noreply.github.com>
2025-06-26 20:59:44 +0200

    Update LoginRegister.tsx

--- src/web/pages/LoginRegister.tsx
+++ src/web/pages/LoginRegister.tsx
@@ -15,21 +15,25 @@ export default ({ mode, page, error }: PropsWithChildren<{ mode: 'login' | 'regi
 				<script src='/login/swap.js' />
 			</Head>
 			<body class='h-full flex flex-center'>
-				<div class='w-min rounded-2xl p-8 dark:bg-stone-800'>
+				<div class='login-container'>
 					<Outlink>
-						<img class='mb-4' src='/favicon.png' />
+						<img class='mb-4 mx-auto' src='/favicon.png' />
 					</Outlink>
 
-					{/* insert Error element if, one was supplied */}
+					{/* Display error message if provided */}
 					{error != null ? Error(`${error}`) : ''}
 
 					{/* Login form */}
 					<form class={mode == 'login' ? '' : 'hidden'} id='login' action={`/login?page=${page ?? 'dashboard'}`} method='post'>
-						<p class='text-xl mt-4'>Username</p>
-						<input name='username' type='text' autofocus class='input-text'></input>
-						<p class='text-xl mt-4'>Password</p>
-						<input name='password' type='password' class='input-text'></input>
-						<div class='flex justify-around mt-4 gap-4 px-2'>
+						<div class='form-row'>
+							<p class='text-xl'>Username</p>
+							<input name='username' type='text' autofocus class='input-text'></input>
+						</div>
+						<div class='form-row'>
+							<p class='text-xl mt-4'>Password</p>
+							<input name='password' type='password' class='input-text'></input>
+						</div>
+						<div class='button-group'>
 							<button class='button basis-full font-bold' type='submit'>Login</button>
 							<button class='button basis-full' type='button' onclick='swap();'>
 								Register
@@ -39,13 +43,19 @@ export default ({ mode, page, error }: PropsWithChildren<{ mode: 'login' | 'regi
 
 					{/* Register form */}
 					<form class={mode == 'register' ? '' : 'hidden'} id='register' action='/register' method='post'>
-						<p class='text-xl mt-4'>Username</p>
-						<input name='username' type='text' autofocus class='input-text'></input>
-						<p class='text-xl mt-4'>Password</p>
-						<input name='password' type='password' class='input-text'></input>
-						<p class='text-xl mt-4'>Confirm Password</p>
-						<input name='password2' type='password' class='input-text'></input>
-						<div class='flex justify-around mt-4 gap-4 px-2'>
+						<div class='form-row'>
+							<p class='text-xl'>Username</p>
+							<input name='username' type='text' autofocus class='input-text'></input>
+						</div>
+						<div class='form-row'>
+							<p class='text-xl mt-4'>Password</p>
+							<input name='password' type='password' class='input-text'></input>
+						</div>
+						<div class='form-row'>
+							<p class='text-xl mt-4'>Confirm Password</p>
+							<input name='password2' type='password' class='input-text'></input>
+						</div>
+						<div class='button-group'>
 							<button class='button basis-full' type='button' onclick='swap();'>Login</button>
 							<button class='button basis-full font-bold' type='submit'>Register</button>
 						</div>
